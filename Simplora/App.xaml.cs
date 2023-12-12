d7c6f332d7673e1ffcd32e05d777cc0e513c6e44
dylandavids0n <dylan.davidson5267@gmail.com>
2023-12-12 12:00:38 -0500

    Added Maui

---
+++ Simplora/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Simplora;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Simplora/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Simplora;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
